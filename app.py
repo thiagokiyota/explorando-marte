ae151a5c4cbabbba1d5ee61262c9fd57869aaf9e
Thiago Kiyota <thiago.kiyota@gmail.com>
2020-12-18 15:38:48 -0300

    Refatoração

--- app.py
+++ app.py
@@ -42,36 +42,18 @@ def mover(x, y, z):
     
 # Parametros de entrada
 area = input()
-sonda1 = input()
-movimentos1 = []
-movimentos1[:0] = input()
-
-sonda2 = input() 
-movimentos2 = []
-movimentos2[:0] = input()
 
 # Tratamento das entradas
 area = area.split()
 
 # Considerando que na perspectiva do usuario o tamanho do planalto vai ter 5 posicoes de 0 a 5 estou incrementando a area para atender a essa expectativa
-p1 = int(area[0])
-p2 = int(area[1])
-p1 = p1 + 1
-p2 = p2 + 1
-
-# Tratamento das entradas
-sonda1 = sonda1.split()
-x1 = int(sonda1[0])
-y1 = int(sonda1[1])
-z1 = sonda1[2].upper()
-
-sonda2 = sonda2.split()
-x2 = int(sonda2[0])
-y2 = int(sonda2[1])
-z2 = sonda2[2].upper()
+largura_plano = int(area[0])
+altura_plano = int(area[1])
+largura_plano = largura_plano + 1
+altura_plano = altura_plano + 1
 
 # Criacao do planalto
-planalto = base(p1, p2)
+planalto = base(largura_plano, altura_plano)
 
 # Posição inicial da sonda1
 # planalto[x, y] = 1
@@ -80,29 +62,31 @@ planalto = base(p1, p2)
 # print("Posicao inicial da sonda2: ", x2, y2, z2)
 
 # Movimentos da sonda1
-for mov in movimentos1:
-    if mov == 'M':
-        aux = mover(x1, y1, z1)
-        x1 = int(aux[0])
-        y1 = int(aux[1])
-        
-    else:
-        z1 = rotate(z1, mov)
+def movimenta_sonda(comandos, x, y, z):
+    for mov in comandos:
+        if mov == 'M':
+            posicao_final = mover(x, y, z)
+            if posicao_final[0] < largura_plano and posicao_final[1] < altura_plano:
+                x = int(posicao_final[0])
+                y = int(posicao_final[1])
+            else:
+                raise Exception("Movimento inválido, sem execução")
+        else:
+            z = rotate(z, mov)
 
-planalto[x1, y1] = 1
-print(x1, y1, z1)
+    planalto[x, y] = 1
+    print(x, y, z)
 
-# Movimentos da sonda2
-for mov in movimentos2:
-    if mov == 'M':
-        aux = mover(x2, y2, z2)
-        x2 = int(aux[0])
-        y2 = int(aux[1])
-        
-    else:
-        z2 = rotate(z2, mov)
+entrada = input("Digite a posição da nova sonda ou CTRL + C\n")
 
-planalto[x2, y2] = 2
-print(x2, y2, z2)
+while entrada != "S":
+    entrada = entrada.split()
+    x1 = int(entrada[0])
+    y1 = int(entrada[1])
+    z1 = entrada[2].upper()
+    movimentos = []
+    movimentos[:0] = input()
+    movimenta_sonda(movimentos, x1, y1, z1)
+    entrada = input("Digite a posição da nova sonda ou CTRL + C\n")
 
 # print(np.rot90(planalto))
